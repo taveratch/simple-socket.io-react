a23ea21918cbb517e9cea867947e2aa2f3408347
TAWEESOFT <TAWEESOFT@TAWEESOFTs-MacBook-Pro.local>
2016-07-22 22:29:03 +0700

    Added redux react-redux hot-reload

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,25 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  entry: './main.js',
-  output: { path: __dirname, filename: 'bundle.js' },
-  module: {
-    loaders: [
-      {
-        test: /.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'react']
-        }
-      }
+	entry: './main.js',
+	output: {
+		path: __dirname,
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /.jsx?$/,
+				loader: ['react-hot', 'babel', 'babel-loader'],
+				exclude: /node_modules/,
+				query: {
+					presets: ['es2015', 'react']
+				}
+      },
+			{
+				test: path.join(__dirname, 'es6'),
+				loader: 'babel-loader'
+			}
     ]
-  },
+	},
 };
