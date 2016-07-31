2715e607b57f990484078011fd18e466bb8ce5e4
TAWEESOFT <TAWEESOFT@TAWEESOFTs-MacBook-Pro.local>
2016-07-31 19:03:31 +0700

    fix internal libraries

--- libs/exceed.lib.js
+++ libs/exceed.lib.js
@@ -1,17 +1,14 @@
 (function() {
   'use strict';
-  var lib = {
-    EXcreateMessage: function(message, username) {
-      return {
-        id: new Date().getTime(),
-        user: username,
-        message: message
-      };
-    },
-    EXformat: function(arr) {
-      var str = JSON.stringify(arr);
-      return str.substring(0,str.length);
-    }
+  window.EXcreateMessage = function(message, username) {
+    return {
+      id: new Date().getTime(),
+      user: username,
+      message: message
+    };
   };
-  module.exports = lib;
+  window.EXformat = function(arr) {
+    var str = JSON.stringify(arr);
+    return str.substring(0,str.length);
+  };
 }());
